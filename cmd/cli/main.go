56a885c98f94630054d273c50578352068319187
Chris Wojno <chris@wojno.com>
2020-12-31 20:00:06 -0800

    updog script pushed

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const upDog = "     __  \n" +
+	"(___()'`;\n" +
+	"/,    /` \n" +
+	`\\"--\\  `
+
+func dogSay(say string) {
+	indent := strings.Repeat(" ", len(strings.Split(upDog, "\n")[0]))
+	topBorder := strings.Repeat("-", len(say)+2)
+	fmt.Println(indent + "┌" + topBorder + "┐")
+	fmt.Println(indent + "| " + say + " |")
+	fmt.Println(indent + "v" + topBorder + "┘")
+	fmt.Println(upDog)
+}
 
 func main() {
-	fmt.Println("Hi! I am Go and I love you!")
+	dogSay("Hi! I am Go and I love you!")
 }
