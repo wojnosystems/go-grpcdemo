e370428e9d7a04e8401d6c179413329e9a33afb1
Chris Wojno <chris@wojno.com>
2020-12-31 19:59:33 -0800

    script rename, fixed from using local to remote docker image. fixed path issues

--- script/compile.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm -v $(pwd):/code golang:1.15 go run /code/cmd/cli/main.go
--- script/editor.bash
+++ script/editor.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ROOT=$(dirname "$1")
-docker run --rm -ti -v $(pwd)/"$ROOT":/"$ROOT" -v $(pwd)/typewriter:/typewriter go-typewriter:latest -- /"$1"
+docker run --rm -ti --user $(id -u):$(id -g) -v $(pwd)/"$ROOT":/"$ROOT" -v $(pwd)/typewriter:/typewriter cwojno/go-typewriter:latest -- /"$1"
